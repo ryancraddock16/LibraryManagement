5b7adfbf2f2d78f37ab4206959097c6d82bb8abe
UltraPheonix <UltraPheonix@users.noreply.github.com>
2024-12-05 17:14:33 +0000

    deploy: ca45a23074b59ce58aa6e25ee7ad447dd44f774a

--- search/all_b.js
+++ search/all_b.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['removebook_0',['removeBook',['../classInventory.html#a75431d03b667d5b1e638bb8f49618064',1,'Inventory']]],
-  ['removebookfrominventory_1',['removeBookFromInventory',['../classLibrarian.html#a3d535828d4b603e12b2751c61cdd191c',1,'Librarian']]],
+  ['removebook_0',['removeBook',['../classInventory.html#a53688860be61a72a3b887224715996c5',1,'Inventory::removeBook(long long ISBN) const'],['../classInventory.html#a75431d03b667d5b1e638bb8f49618064',1,'Inventory::removeBook(const Book *b) const']]],
+  ['removebookfrominventory_1',['removeBookFromInventory',['../classLibrarian.html#a1274a0da3ca11a342e73eac7e01f54c7',1,'Librarian::removeBookFromInventory(long long ISBN)'],['../classLibrarian.html#a3d535828d4b603e12b2751c61cdd191c',1,'Librarian::removeBookFromInventory(Book *book) const']]],
   ['renewbook_2',['renewBook',['../classLibrarian.html#ac7b8643a53c31045baffad6bcd29d03e',1,'Librarian']]],
   ['reservations_3',['reservations',['../classLibrarian.html#a84487289418cf7f6a32d8063cb8b5a51',1,'Librarian']]],
   ['reservebook_4',['reserveBook',['../classLibrarian.html#ae18939f8e8972d327a532a7f77a73be6',1,'Librarian']]],

9864b4c53fa49ecc693dba18db251a5c914ce1fd
UltraPheonix <UltraPheonix@users.noreply.github.com>
2024-12-15 15:57:52 +0000

    deploy: 85ebde8a4535e8296e657ecafb54cdba6df74e31

--- search/all_e.js
+++ search/all_e.js
@@ -1,10 +1,11 @@
 var searchData=
 [
   ['references_0',['Pointers and references',['../md_README.html#autotoc_md5',1,'']]],
-  ['removebook_1',['removeBook',['../classInventory.html#a53688860be61a72a3b887224715996c5',1,'Inventory::removeBook(long long ISBN) const'],['../classInventory.html#a75431d03b667d5b1e638bb8f49618064',1,'Inventory::removeBook(const Book *b) const']]],
-  ['removebookfrominventory_2',['removeBookFromInventory',['../classLibrarian.html#a1274a0da3ca11a342e73eac7e01f54c7',1,'Librarian::removeBookFromInventory(long long ISBN)'],['../classLibrarian.html#a3d535828d4b603e12b2751c61cdd191c',1,'Librarian::removeBookFromInventory(Book *book) const']]],
-  ['renewbook_3',['renewBook',['../classLibrarian.html#ac7b8643a53c31045baffad6bcd29d03e',1,'Librarian']]],
-  ['reservations_4',['reservations',['../classLibrarian.html#a84487289418cf7f6a32d8063cb8b5a51',1,'Librarian']]],
-  ['reservebook_5',['reserveBook',['../classLibrarian.html#ae18939f8e8972d327a532a7f77a73be6',1,'Librarian']]],
-  ['returnbook_6',['returnBook',['../classLibrarian.html#a19dbcc897ae6bfc59835b8feaac035f0',1,'Librarian::returnBook(long long ISBN)'],['../classLibrarian.html#a34ef7080595ab5a8d92c94c436869178',1,'Librarian::returnBook(Book *book)']]]
+  ['reflection_1',['Reflection',['../md_README.html#autotoc_md12',1,'']]],
+  ['removebook_2',['removeBook',['../classInventory.html#a53688860be61a72a3b887224715996c5',1,'Inventory::removeBook(long long ISBN) const'],['../classInventory.html#a75431d03b667d5b1e638bb8f49618064',1,'Inventory::removeBook(const Book *b) const']]],
+  ['removebookfrominventory_3',['removeBookFromInventory',['../classLibrarian.html#a1274a0da3ca11a342e73eac7e01f54c7',1,'Librarian::removeBookFromInventory(long long ISBN)'],['../classLibrarian.html#a3d535828d4b603e12b2751c61cdd191c',1,'Librarian::removeBookFromInventory(Book *book) const']]],
+  ['renewbook_4',['renewBook',['../classLibrarian.html#ac7b8643a53c31045baffad6bcd29d03e',1,'Librarian']]],
+  ['reservations_5',['reservations',['../classLibrarian.html#a84487289418cf7f6a32d8063cb8b5a51',1,'Librarian']]],
+  ['reservebook_6',['reserveBook',['../classLibrarian.html#ae18939f8e8972d327a532a7f77a73be6',1,'Librarian']]],
+  ['returnbook_7',['returnBook',['../classLibrarian.html#a19dbcc897ae6bfc59835b8feaac035f0',1,'Librarian::returnBook(long long ISBN)'],['../classLibrarian.html#a34ef7080595ab5a8d92c94c436869178',1,'Librarian::returnBook(Book *book)']]]
 ];
